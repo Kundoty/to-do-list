8845eadd01643e90627eec6fc93a63c543aba3b5
Kundoty <banyuanjundao@gmail.com>
2025-08-04 12:07:17 -0400

    todolist with hooks

--- src/todoList/todoList.jsx
+++ src/todoList/todoList.jsx
@@ -1,29 +1,36 @@
-import { useState } from "react";
+import { useState, useMemo, useCallback } from "react";
 import { v4 as uuidv4 } from "uuid";
+import "./todoList.css";
 
 export default function TodoList() {
   const [inputValue, setInputValue] = useState("");
   const [todoList, setTodoList] = useState([]);
   const [editID, setEditID] = useState("");
   const [editInput, setEditInput] = useState("");
+  const pendingTodo = useMemo(
+    () => todoList.filter((todoItem) => !todoItem.completed),
+    [todoList]
+  );
+  const completTodo = useMemo(
+    () => todoList.filter((todoItem) => todoItem.completed),
+    [todoList]
+  );
 
-  const handleInput = (e) => {
+  const handleInput = useCallback((e) => {
     setInputValue(e.target.value);
-  };
+  }, []);
 
   const handleSubmit = () => {
-    const newTodo = { title: inputValue, id: uuidv4() };
-    console.log(newTodo.id);
-    const newTodoList = [...todoList];
-    newTodoList.push(newTodo);
+    const newTodo = { title: inputValue, completed: false, id: uuidv4() };
+    const newTodoList = [...todoList, newTodo];
     setTodoList(newTodoList);
     setInputValue("");
   };
 
-  const handleDelete = (id) => {
+  const handleDelete = useCallback((id) => {
     const filteredTodoList = todoList.filter((todoItem) => todoItem.id !== id);
     setTodoList(filteredTodoList);
-  };
+  }, []);
 
   const handleSetEdit = ({ title, id }) => {
     setEditID(id);
@@ -47,37 +54,91 @@ export default function TodoList() {
     setEditID("");
   };
 
+  const handleSwitchComplete = (id) => {
+    const updateList = todoList.map((todoItem) =>
+      todoItem.id === id
+        ? {
+            ...todoItem,
+            completed: !todoItem.completed,
+          }
+        : todoItem
+    );
+    setTodoList(updateList);
+  };
+
   return (
     <>
-    <h2>To Do List</h2>
+      <h2>To Do List</h2>
       <div>
         <input type="text" value={inputValue} onChange={handleInput} />
         <button onClick={handleSubmit}>Submit</button>
       </div>
-      <ul>
-        {todoList.map((todoItem) => (
-          <li key={todoItem.id}>
-            {editID === todoItem.id ? (
-              <>
-                <input
-                  type="text"
-                  value={editInput}
-                  onChange={handleEditInput}
-                />
-                <button onClick={() => handleSave(todoItem.id)}>Save</button>
-              </>
-            ) : (
-              <>
-                {todoItem.title}
-                <button onClick={() => handleDelete(todoItem.id)}>
-                  Delete
-                </button>
-                <button onClick={() => handleSetEdit(todoItem)}>Edit</button>
-              </>
-            )}
-          </li>
-        ))}
-      </ul>
+      <div className="todo-list-section">
+        <div className="pending-todo">
+          <ul>
+            <span>Pending Todo</span>
+            {pendingTodo.map((todoItem) => (
+              <li key={todoItem.id}>
+                {editID === todoItem.id ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editInput}
+                      onChange={handleEditInput}
+                    />
+                    <button onClick={() => handleSave(todoItem.id)}>
+                      Save
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    {todoItem.title}
+                    <button onClick={() => handleDelete(todoItem.id)}>
+                      Delete
+                    </button>
+                    <button onClick={() => handleSetEdit(todoItem)}>
+                      Edit
+                    </button>
+                    <button onClick={(() => handleSwitchComplete(todoItem.id))}>{'=>'}</button>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="complete-todo">
+          <ul>
+            <span>Complete Todo</span>
+            {completTodo.map((todoItem) => (
+              <li key={todoItem.id}>
+                {editID === todoItem.id ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editInput}
+                      onChange={handleEditInput}
+                    />
+                    <button onClick={() => handleSave(todoItem.id)}>
+                      Save
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button onClick={() => handleSwitchComplete(todoItem.id)}>{'<='}</button>
+                    {todoItem.title}
+                    <button onClick={() => handleDelete(todoItem.id)}>
+                      Delete
+                    </button>
+                    <button onClick={() => handleSetEdit(todoItem)}>
+                      Edit
+                    </button>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
